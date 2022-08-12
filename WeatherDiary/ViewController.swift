1888df5264ccfb6b305648b866f5c470cf15ecbd
Glsme <glasses.str.man@gmail.com>
2022-08-12 17:15:48 +0900

    Initial Commit

---
+++ WeatherDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherDiary
+//
+//  Created by Seokjune Hong on 2022/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

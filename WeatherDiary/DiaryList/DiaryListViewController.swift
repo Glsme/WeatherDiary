324d6b6640e3846c4b1232b3983e48abaae2fa02
Glsme <glasses.str.man@gmail.com>
2022-08-13 22:53:35 +0900

    Feat: DiaryList 관련 파일 추가

---
+++ WeatherDiary/DiaryList/DiaryListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryListViewController.swift
+//  WeatherDiary
+//
+//  Created by Seokjune Hong on 2022/08/13.
+//
+
+import UIKit
+
+class DiaryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ WeatherDiary/Model/StoryboardName.swift
@@ -0,0 +1,12 @@
+//
+//  StoryboardName.swift
+//  WeatherDiary
+//
+//  Created by Seokjune Hong on 2022/08/13.
+//
+
+import Foundation
+
+enum StoryboardName: String {
+    case Weather, DiaryList
+}

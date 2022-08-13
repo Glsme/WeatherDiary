8d0751ae0817f9d62b0e56e6188fd20ce77e47aa
Glsme <glasses.str.man@gmail.com>
2022-08-13 23:00:16 +0900

    Feat: ReusableViewProtocol 파일 추가 및 적용 코드 추가

---
+++ WeatherDiary/Protocol/ReusableViewProtocolswift.swift
@@ -0,0 +1,19 @@
+//
+//  ReusableProtocolswift.swift
+//  WeatherDiary
+//
+//  Created by Seokjune Hong on 2022/08/13.
+//
+
+import Foundation
+import UIKit
+
+protocol ReusableProtocl {
+    static var reuseIdentifier: String { get }
+}
+
+extension UIViewController: ReusableProtocl {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
